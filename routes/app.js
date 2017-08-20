d69de0904e28c6a4d039ce5906ac6413be17cbd3
Rohan Thorat <rthorat1@binghamton.edu>
2017-08-20 08:47:32 +0530

    Fetching data from mongodb

--- routes/app.js
+++ routes/app.js
@@ -4,7 +4,13 @@ var User = require('../models/user');
 
 
 router.get('/', function (req, res, next) {
-    res.render('node');
+    User.findOne({}, function(err, doc){
+        if (err) {
+            return res.send('Error!');
+        }
+        res.render('node',{email: doc.email});
+    });
+
 });
 
 router.post('/', function (req, res, next) {
