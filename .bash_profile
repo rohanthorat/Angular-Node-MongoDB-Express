bc1e2c0da1e999ea2f6a226079720dc0883ea2aa
Rohan Thorat <rthorat1@binghamton.edu>
2017-08-20 07:55:15 +0530

    Node server and Mongoose setup

---
+++ .bash_profile
@@ -0,0 +1,1 @@
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/Rohan/Documents/Binghamton/semester2/DP/Assignments/1/ANT/apache-ant-1.10.1/bin:/Users/Rohan/Documents/udemy/Angular/fullstack/mongodb/bin
---
+++ models/message.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Rohan on 8/20/17.
+ */
+/**
+ * Created by Rohan on 8/20/17.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    content: {type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Message', schema);
